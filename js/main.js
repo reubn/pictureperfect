cf236c0992692deb1af9265ac103be3a274d9423
reubn <reuben@t3ch.tk>
2013-07-12 21:26:00 +0100

    Added Google Maps

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-
+// Enable the visual refresh
+google.maps.visualRefresh = true;
+var mapProp = {
+    center: new google.maps.LatLng(52.0500, -2.7167),
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    disableDefaultUI: true,
+    panControl: false,
+  zoomControl: false,
+  mapTypeControl: true,
+  scaleControl: false,
+  streetViewControl: true,
+  overviewMapControl: false
+    };
+  map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
